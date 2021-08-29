3428307523ebd0602667b8275befb57eb2b366f7
Cary <56490230+CaryRandazzo@users.noreply.github.com>
2021-08-28 21:21:28 -0500

    Update replica_download_script.py

--- File_Imports/replica_download_script.py
+++ File_Imports/replica_download_script.py
@@ -1,1 +1,17 @@
+def proc_rucDL_text(filename):
+  
+    """
+    
+    Processes the text file containing lines of rucio download files in the form <scope>:<did>, one per line
+    
+    """
 
+    with open(filename, "r+") as f:
+        # here, position is initially at the beginning
+        text = f.read()
+        # now its at the end of this file
+    with open(filename+"_processed",'w') as f:
+        for line in text.split('\n'):
+            f.write('rucio download '+ line + '\n')
+
+    return
