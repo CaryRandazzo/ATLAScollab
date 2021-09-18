f70f19efcf5fc2a821b0412bb61e8061875d81ff
Cary <56490230+CaryRandazzo@users.noreply.github.com>
2021-09-18 16:57:29 -0500

    Update ML_imports.py

--- File_Imports/ML_imports.py
+++ File_Imports/ML_imports.py
@@ -1,11 +1,11 @@
 # Use this file as shortcut import to import all Machine Learning imports that you may need in a project
 
-#Processing
+# Processing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles, make_classification
 
-#Unsupervised learning
+# Unsupervised Learning - sklearn
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -15,8 +15,10 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+# Unsupervised Learning - pyod
 from pyod.models.auto_encoder import AutoEncoder
-# from pyod.models.feature_bagging import FeatureBagging
+from pyod.models.feature_bagging import FeatureBagging
 from pyod.models.knn     import KNN
 from pyod.models.lof     import LOF
 from pyod.models.hbos    import HBOS
