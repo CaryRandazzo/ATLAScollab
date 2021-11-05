a255dbeac94649e509851547cd75883059549424
Cary <56490230+CaryRandazzo@users.noreply.github.com>
2021-11-05 00:24:25 -0500

    Update default_imports.py

--- AtlasNet/File_Imports/default_imports.py
+++ AtlasNet/File_Imports/default_imports.py
@@ -6,7 +6,7 @@ import ROOT
 
 # Collection of standard packages (matplotlib, pandas, numpy, etc)
 import pyforest
-
+import pandas as pd # Specifically as pd because pyforest doesnt import with these shorthands
 
 # Other packages
 from IPython.display import clear_output
